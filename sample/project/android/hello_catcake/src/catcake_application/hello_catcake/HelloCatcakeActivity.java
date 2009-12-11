94e304b7bfdb93f4a82bbbc20450f2e178d36176
takashi.kitao <takashi.kitao@2194337e-c11f-11de-8907-fd8f2f62d6ab>
2009-12-11 06:20:51 +0000

    git-svn-id: https://catcake.googlecode.com/svn/trunk@156 2194337e-c11f-11de-8907-fd8f2f62d6ab

---
+++ sample/project/android/hello_catcake/src/catcake_application/hello_catcake/HelloCatcakeActivity.java
@@ -0,0 +1,13 @@
+package catcake_application.hello_catcake;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloCatcakeActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
